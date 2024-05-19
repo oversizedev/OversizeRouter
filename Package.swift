bc709dc55823c772030abd60280638292baa1f34
Alexander Romanov <aromanov07@gmail.com>
2024-05-19 16:03:24 +0300

    Fix tvOS

--- Package.swift
+++ Package.swift
@@ -8,14 +8,15 @@ let package = Package(
     platforms: [
         .iOS(.v16),
         .macOS(.v13),
-        .tvOS(.v15),
+        .tvOS(.v16),
         .watchOS(.v9),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OversizeRouter",
-            targets: ["OversizeRouter"]),
+            targets: ["OversizeRouter"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,6 +25,7 @@ let package = Package(
             name: "OversizeRouter"),
         .testTarget(
             name: "OversizeRouterTests",
-            dependencies: ["OversizeRouter"]),
+            dependencies: ["OversizeRouter"]
+        ),
     ]
 )
--- Tests/OversizeRouterTests/OversizeRouterTests.swift
+++ Tests/OversizeRouterTests/OversizeRouterTests.swift
@@ -1,5 +1,5 @@
-import XCTest
 @testable import OversizeRouter
+import XCTest
 
 final class OversizeRouterTests: XCTestCase {
     func testExample() throws {
