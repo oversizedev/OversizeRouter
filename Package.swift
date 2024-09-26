415056e9a63fff42c7e9cbfca4086db3373e6e33
Alexandr Romanov <aromanov07@gmail.com>
2024-09-27 00:18:39 +0300

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -2,8 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
-let productionDependencies: [PackageDescription.Package.Dependency] = [
+let remoteDependencies: [PackageDescription.Package.Dependency] = [
     .package(url: "https://github.com/oversizedev/OversizeUI.git", .upToNextMajor(from: "3.0.2")),
     .package(url: "https://github.com/oversizedev/OversizeServices.git", .upToNextMajor(from: "1.4.0")),
     .package(url: "https://github.com/oversizedev/OversizeModels.git", .upToNextMajor(from: "0.1.0")),
@@ -11,7 +12,7 @@ let productionDependencies: [PackageDescription.Package.Dependency] = [
     .package(url: "https://github.com/oversizedev/OversizeLocalizable.git", .upToNextMajor(from: "1.5.0")),
 ]
 
-let developmentDependencies: [PackageDescription.Package.Dependency] = [
+let localDependencies: [PackageDescription.Package.Dependency] = [
     .package(name: "OversizeUI", path: "../OversizeUI"),
     .package(name: "OversizeServices", path: "../OversizeServices"),
     .package(name: "OversizeModels", path: "../OversizeModels"),
@@ -19,7 +20,11 @@ let developmentDependencies: [PackageDescription.Package.Dependency] = [
     .package(url: "https://github.com/hmlongco/Factory.git", .upToNextMajor(from: "2.1.3")),
 ]
 
-let dependencies: [PackageDescription.Package.Dependency] = developmentDependencies
+var dependencies: [PackageDescription.Package.Dependency] = localDependencies
+
+if ProcessInfo.processInfo.environment["BUILD_MODE"] == "PRODUCTION" {
+    dependencies = remoteDependencies
+}
 
 let package = Package(
     name: "OversizeRouter",
